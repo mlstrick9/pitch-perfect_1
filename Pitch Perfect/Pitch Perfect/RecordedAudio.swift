ea535a3c28e22b3c14810a9d914665378f496d98
mlstrick9 <mlstrick9@users.noreply.github.com>
2015-04-27 20:01:27 -0500

    Committed by Udacity

---
+++ Pitch Perfect/Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,14 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by Matthew Strickland on 4/27/15.
+//  Copyright (c) 2015 Matthew Strickland. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject{
+    var filePathUrl: NSURL!
+    var title: String!
+}
